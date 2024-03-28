91e85920f9207d9caec92fd9e0cea3eba5cf7735
yuliiasida <yulia.swr@gmail.com>
2024-03-28 14:56:54 +0200

    web views

---
+++ src/main/resources/static/schema.sql
@@ -0,0 +1,29 @@
+-- -- Accounts Table
+-- CREATE TABLE accounts
+-- (
+--     id           SERIAL PRIMARY KEY,
+--     first_name   VARCHAR(50)  NOT NULL,
+--     last_name    VARCHAR(50)  NOT NULL,
+--     email        VARCHAR(100) NOT NULL,
+--     password     VARCHAR(255) NOT NULL,
+--     avatar_image VARCHAR(255)
+-- );
+--
+-- -- Set Table
+-- CREATE TABLE sets
+-- (
+--     id      SERIAL PRIMARY KEY,
+--     name    VARCHAR(100) NOT NULL,
+--     user_id INT          NOT NULL,
+--     FOREIGN KEY (user_id) REFERENCES accounts (id)
+-- );
+--
+-- -- Flashcard Table
+-- CREATE TABLE flashcard
+-- (
+--     id     SERIAL PRIMARY KEY,
+--     front  TEXT NOT NULL,
+--     back   TEXT NOT NULL,
+--     set_id INT  NOT NULL,
+--     FOREIGN KEY (set_id) REFERENCES sets (id)
+-- );
