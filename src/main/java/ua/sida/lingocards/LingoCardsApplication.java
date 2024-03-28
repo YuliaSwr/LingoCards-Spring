72154830f64def455704bdb4ab7c4fe7ae3ec1a7
yuliiasida <yulia.swr@gmail.com>
2024-03-28 14:41:52 +0200

    Initial commit

---
+++ src/main/java/ua/sida/lingocards/LingoCardsApplication.java
@@ -0,0 +1,13 @@
+package ua.sida.lingocards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoCardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingoCardsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/sida/lingocards/LingoCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.sida.lingocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoCardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
