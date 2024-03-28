ca7e8ff3b94de1dceddb93ccfb18ea2ea023e782
yuliiasida <yulia.swr@gmail.com>
2024-03-28 14:56:30 +0200

    enabled caching

--- src/main/java/ua/sida/lingocards/LingoCardsApplication.java
+++ src/main/java/ua/sida/lingocards/LingoCardsApplication.java
@@ -2,12 +2,21 @@ package ua.sida.lingocards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class LingoCardsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LingoCardsApplication.class, args);
     }
 
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("translations");
+    }
 }
